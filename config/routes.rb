972ef8cdb678830bea2515fa92fcfafc1060706d
kpb <email@example.com>
2018-09-09 11:38:18 +0900

    Finish password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_reset/new'
+
+  get 'password_reset/edit'
+
   root 'static_pages#home'
   get  '/about',   to: 'static_pages#about'
   get  '/help',    to: 'static_pages#help'
@@ -11,5 +15,6 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update] 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
